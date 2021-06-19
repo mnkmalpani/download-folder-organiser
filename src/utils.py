8eb3eb3e66bb6453c15528186264e0e3d438e818
Mayank Malpani <mmalpani@atlassian.com>
2021-06-19 11:33:19 +0530

    fix: add other folder creation optional

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ def check_folder_exists(new_file_folder):
     return os.path.isdir(new_file_folder)
 
 def move_files(folder_to_track, new_file_folder, extention_dict):
-    file_extension_found = False
+    file_extension_found, should_create_other = False, False
     if not check_folder_exists(new_file_folder):
         os.mkdir(new_file_folder)
     for filename in os.listdir(folder_to_track):
@@ -26,7 +26,7 @@ def move_files(folder_to_track, new_file_folder, extention_dict):
                             os.makedirs(sub_folder)
                     shutil.copy(source_file_name, sub_folder)
                     file_extension_found = True
-            if not file_extension_found:
+            if not file_extension_found and should_create_other:
                 sub_folder = sub_folder + '/others'
                 if not check_folder_exists(sub_folder):
                     os.mkdir(sub_folder)
